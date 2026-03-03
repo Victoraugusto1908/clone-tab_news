7c0681596a3b656a3a203eaa85bfcb166400328f
Victoraugusto1908 <victor.augusto@estudante.ufjf.br>
2026-03-02 21:56:31 -0300

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -9,9 +9,46 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(queryObject);
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
+}
+
+async function postgresMaxConnections() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.getMaxListeners();
   await client.end();
   return result;
 }
 
-export default { query: query };
+async function postgresConnections() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.listenerCount();
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+  connections: postgresConnections,
+  maxConnections: postgresMaxConnections,
+};
